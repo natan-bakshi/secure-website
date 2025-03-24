9f86e4132a826e9b346bd028a75c5bd2cc5d8379
natib <natib8000@gmai.com>
2025-03-24 10:41:07 +0200

    commit 1.0

--- app.py
+++ app.py
@@ -1,66 +1,8 @@
 from flask import Flask, render_template
-from flask import request, redirect, url_for
-import sqlite3
-import uuid
+from flask import request
+from helper import *
 
-
-BAD_CHARS = {' ', "'", '"', ';', '--', '<', '>','/', '='}
 app = Flask(__name__)
-
-import sqlite3
-
-def print_users():
-    connection = sqlite3.connect('users.db')
-    c = connection.cursor()
-    c.execute("SELECT * FROM users")
-    users = c.fetchall()
-    for user in users:
-        print(user)
-    connection.close()
-
-
-def check_user(username):
-    connection = sqlite3.connect('users.db')
-    c = connection.cursor()
-    c.execute("SELECT username FROM users WHERE username = ?", (username,))
-    user = c.fetchone()
-
-    connection.close()
-
-    if user:
-        return True
-    return False
-
-def check_user_login(username, password):
-    if any(bad_char in password for bad_char in BAD_CHARS):
-        return "Password contains invalid characters"
-
-    connection = sqlite3.connect('users.db')
-    c = connection.cursor()
-    c.execute("SELECT password FROM users WHERE username = ?", (username,))
-    user_password = c.fetchone()
-
-    connection.close()
-
-    if user_password and user_password[0] == password:
-        return True
-    return False
-
-
-def create_user(username, password):
-    if any(bad_char in password for bad_char in BAD_CHARS):
-        return "Password contains invalid characters"
-
-    connection = sqlite3.connect('users.db')
-    c = connection.cursor()
-
-    sql_execute_string = "INSERT INTO users(Uid, username, password) VALUES (?, ?, ?)"
-    user_id = str(uuid.uuid4())
-
-    c.execute(sql_execute_string, (user_id, username, password))
-    connection.commit()
-    connection.close()
-
 
 
 @app.route('/')
@@ -75,41 +17,39 @@ def sign_up():
         username = request.form['username']
         password1 = request.form['password1']
         password2 = request.form['password2']
-        if username and password1 and password2:
-            if password1 == password2:
-                if not check_user(username):
-                    create_user(username, password1)
-                    return render_template('home.html')
-                else:
-                    return "Username taken"
-            return "Mismatched passwords"
-        return "Please fill in all fields"
+        if check_characters(password1):
+            if username and password1 and password2:
+                if password1 == password2:
+                    if not check_user(username):
+                        create_user(username, password1)
+                        return render_template('home.html')
+                    else:
+                        return "Username taken"
+                return "Mismatched passwords"
+            return "Please fill in all fields"
+        return
     return render_template('sign_up.html')
 
 
 @app.route('/Login', methods=['post', 'get'])
 def log_in():
-    print(request.method)
+    error_message = ""
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
-        if check_user_login(username, password):
+        if not check_characters(password):
+            error_message = "you ar very naughty"
+
+        elif  not check_user_login(username, password):
+            error_message = "The username or password is incorrect"
+
+        else:
             return render_template('home.html')
-        return "Incorrect username or password"
-    else:
-        return render_template('log_in.html')
+
+    return render_template('log_in.html',message = error_message)
 
 
-
-# def query_contact_details():
-#     connie = sqlite3.connect(db_locale)
-#     c = connie.cursor()
-#     c.execute("""
-#     SELECT * FROM contact_details
-#     """)
-#     student_data = c.fetchall()
-#     return student_data
 
 
 if __name__ == '__main__':
---
+++ helper.py
@@ -0,0 +1,58 @@
+import sqlite3
+import uuid
+BAD_CHARS = {" ", "'", '"', ';', '--', '<', '>','/', '=', '&', ';'}
+
+
+def print_users():
+    connection = sqlite3.connect('users.db')
+    c = connection.cursor()
+    c.execute("SELECT * FROM users")
+    users = c.fetchall()
+    for user in users:
+        print(user)
+    connection.close()
+
+
+def check_user(username):
+    connection = sqlite3.connect('users.db')
+    c = connection.cursor()
+    c.execute("SELECT username FROM users WHERE username = ?", (username,))
+    user = c.fetchone()
+
+    connection.close()
+
+    if user:
+        return True
+    return False
+
+def check_characters(string):
+    if any(bad_char in string for bad_char in BAD_CHARS):
+        return False
+    return True
+
+
+def check_user_login(username, password):
+
+    connection = sqlite3.connect('users.db')
+    c = connection.cursor()
+    c.execute("SELECT password FROM users WHERE username = ?", (username,))
+    user_password = c.fetchone()
+
+    connection.close()
+
+    if user_password and user_password[0] == password:
+        return True
+    return False
+
+
+def create_user(username, password):
+    connection = sqlite3.connect('users.db')
+    c = connection.cursor()
+
+    sql_execute_string = "INSERT INTO users(Uid, username, password) VALUES (?, ?, ?)"
+    user_id = str(uuid.uuid4())
+
+    c.execute(sql_execute_string, (user_id, username, password))
+    connection.commit()
+    connection.close()
+
